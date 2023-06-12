739231a9e5e9bd36ddf071127f4025a9f21bd61a
SHaYanxALyan <100460319+SHaYanxALyan@users.noreply.github.com>
2023-06-13 00:53:05 +0500

    Add files via upload

---
+++ SAFI.py
@@ -0,0 +1,23 @@
+#DEC BY PARADOX
+#GITHUB PARADOX-XD
+#KIDS U7P4L-IN PY3 ENC V2
+import marshal
+import os
+os.system('clear')
+print('\x1b[1;92mFolllow Me On Github ')
+os.system('xdg-open https://github.com/U7P4L-IN ')
+os.system('clear')
+logo = '\x1b[1;97m\n\n\x1b[1;91m   __  __ ______ ____   ___     __         ____ _   __\n\x1b[1;91m  / / / //_  __// __ \\ /   |   / /        /  _// | / /\n\x1b[38;5;208m / / / /  / /  / /_/ // /| |  / /         / / /  |/ / \n\x1b[38;5;208m/ /_/ /  / /  / ____// ___ | / /___     _/ / / /|  /  \n\x1b[1;92m\\____/  /_/  /_/    /_/  |_|/_____/    /___//_/ |_/   \n\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m•\x1b[38;5;46m•\x1b[38;5;196m•\x1b[1;35m•\x1b[1;34m•\x1b[1;33m•\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m•\x1b[1;35m•\x1b[1;34m•\x1b[1;33m•\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m•\x1b[1;32m•\x1b[1;35m•\x1b[1;34m•\x1b[1;97m•\x1b[1;33m•\x1b[38;5;196m•\x1b[1;35m•\x1b[1;34m•\x1b[1;33m•\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m•\x1b[38;5;196m•\x1b[1;32m•\x1b[1;97m•\x1b[1;35m•\x1b[1;34m•\x1b[1;33m•\x1b[38;5;46m•\x1b[1;97m•\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m••\x1b[1;34m•\x1b[1;33m•\x1b[38;5;46m•\x1b[1;97m•\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m•\n\x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]    \x1b[1;32mCREATED BY    \x1b[1;31m:  \x1b[1;94mANONYMOUS U7P4L           \x1b[1;92m \x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]\n\x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]    \x1b[1;32mON GITHUB     \x1b[1;31m:  \x1b[1;94mU7P4L-IN                 \x1b[38;5;46m  \x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]\n\x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]    \x1b[1;32mNETWORK       \x1b[1;31m:  \x1b[1;94m4G,5G \x1b[1;92m                     \x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]\n\x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]    \x1b[1;32mVERSION       \x1b[1;31m:  \x1b[1;94mALPHA PRO \x1b[1;92m                 \x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]\n\x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]    \x1b[1;32mTOOLS NAME    \x1b[1;31m:  \x1b[1;94mENC_PY3                   \x1b[38;5;46m \x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]\n\x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]    \x1b[1;32mTOOLS STATUS  \x1b[1;31m:  \x1b[1;94mFREE \x1b[1;92m                      \x1b[1;97m[\x1b[1;92m❯\x1b[1;97m]\n\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m•\x1b[38;5;46m•\x1b[38;5;196m•\x1b[1;35m•\x1b[1;34m•\x1b[1;33m•\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m•\x1b[1;35m•\x1b[1;34m•\x1b[1;33m•\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m•\x1b[1;32m•\x1b[1;35m•\x1b[1;34m•\x1b[1;97m•\x1b[1;33m•\x1b[38;5;196m•\x1b[1;35m•\x1b[1;34m•\x1b[1;33m•\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m•\x1b[38;5;196m•\x1b[1;32m•\x1b[1;97m•\x1b[1;35m•\x1b[1;34m•\x1b[1;33m•\x1b[38;5;46m•\x1b[1;97m•\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m••\x1b[1;34m•\x1b[1;33m•\x1b[38;5;46m•\x1b[1;97m•\x1b[1;32m•\x1b[1;97m•\x1b[38;5;196m•\x1b[38;5;46m•\n'
+os.system('clear')
+print(logo)
+print('\x1b[31m[-] Example > /sdcard/File.py')
+kun = input('\x1b[1;97m(+) INPUT YOUR FILE : ')
+fileopen = open(kun, 'rb').read()
+a = compile(fileopen, 'dg', 'exec')
+m = marshal.dumps(a)
+s = repr(m)
+c = input('\x1b[1;97m(+) OUTPUT FILE : ')
+d = open(c, 'w').write('##-------------------INFO------------------##\n#ENCRYPTED BY : U7P4L IN\n# VERSION : 0.3\n# OWNER OF ANONYMOUS CYBER\n# FEEL THE POWER OF U7P4L IN\n# GITHUB : https://github.com/U7P4L-IN\n##----------------MAIN>MENU----------------##\n\nimport marshal\nexec(marshal.loads(' + s + '))')
+print('\n\x1b[1;97m(+) Finished...')
+print('\x1b[1;97m(+) File Stored In : ' + c)
+print('')
